d23520deccd6909067af9dcba6fe3cc3787f2af6
derektomassijr <derektomassi@me.com>
2024-09-28 15:51:13 -0400

    Redesigned how the command handler and messages are relayed - also had major logic changed and added new functions for cleanup

--- cs_bot.py
+++ cs_bot.py
@@ -11,8 +11,25 @@ class cs_bot():
         print("Text Channel Detected: " + self.channel.name)
         print("User Active Voice Channel Detected: " + self.voice_channel.name)
 
+    def create_private_match_message(self, team_t, team_ct):
+        pm_message = "=== T Side ===\n"
+        for memeber in team_t:
+            pm_message += (member.name + "\n")
+        
+        pm_message += "\n=== CT Side ===\n"
+        for member in team_ct:
+            pm_message += (member.name + "\n")
+        
+        pm_message += "Type !csconfirmmatch to confirm teams\n"
+        pm_message += "Type !csreroll to reroll teams"
+        
+        return pm_message
+
     def private_match(self, player_count):
-        self.player_count = player_count
+        if (self.match_started):
+            return "A match is currently in progress"
+        
+        self.player_count = len(self.voice_channel.members)
 
         print("Generating Teams...")
         users = self.voice_channel.members
@@ -22,7 +39,8 @@ class cs_bot():
         if (users == None):
             print("No Valid Players Found!!!")
             print("Terminating Game!")
-            return None
+            return """No Users Found in Voice Chat or User is not in Voice Chat!!!\n
+                    Game Not Created!"""
 
         # Splitting the teams
         half_index = round(len(users) / 2)
@@ -33,13 +51,7 @@ class cs_bot():
         print("CT Side Team: ", team_ct)
         game_setup = True
 
-        await self.channel.send("T Side:")
-        for member in team_t:
-            await self.channel.send(member.name)
-
-        await self.channel.send("CT Side:")
-        for member in team_ct:
-            await self.channel.send(member.name)
+        return self.create_private_match_message(team_t, team_ct)
 
     def start_match(self):
         for member in team_t:
@@ -48,16 +60,30 @@ class cs_bot():
         for member in team_ct:
             member.move_to(TEAM_CT_VC, VC_MOVE_REASON)
 
+        return "Match Started"
+
+    def end_match(self):
+        if (game_setup or match_started):
+            game_setup = False
+            match_started = False
+            return "Ended Current Game"
+        else:
+            return """A game has not been created\n
+                   Use the !csprivatematch command to start a game"""
+
     def command_handler(self, command):
-        if (self.PRIVATE_MATCH_COMMAND in command):
-            player_count = command[16:]
-            self.private_match(int(player_count))
+        if (self.command == self.PRIVATE_MATCH_COMMAND or self.command == self.REROLL_TEAMS_COMMAND):
+            return self.private_match()
         elif (self.command == CONFIRM_TEAMS_COMMAND and game_setup):
-            self.start_match()
+            return self.start_match()
+        elif (self.command == END_GAME_COMMAND)
+            return self.end_match()
+
 
     # Initialization Variables
     player_count = 0 # num of players in game
-    game_setup = False # flag for match status
+    game_setup = False # flag for game status
+    match_started = False # flag for match status
     team_t = [] # Team 1
     team_ct = [] # Team 2
     channel = None # message text channel
@@ -69,6 +95,5 @@ class cs_bot():
     VC_MOVE_REASON = "CS Private Match" # Audit Log Move Reason
     PRIVATE_MATCH_COMMAND = "!csprivatematch"
     CONFIRM_TEAMS_COMMAND = "!csconfirmteams"
-
-
-        
+    REROLL_TEAMS_COMMAND = "!csreroll"
+    END_GAME_COMMAND = "!csendgame"
