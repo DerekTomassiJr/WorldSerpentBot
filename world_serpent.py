8456dffc8f4666edb479cdb1d30706417e5c3667
DESKTOP-OBAFPM8\Abrow <Abrowndog11@gmail.com>
2025-06-24 20:58:43 -0400

    Implemented Siege Bot/More Luca commands

--- world_serpent.py
+++ world_serpent.py
@@ -1,6 +1,6 @@
 import discord
 import cs_bot
-from ChildBots import siege_bot
+from SiegeBotFiles import siege_bot
 from bot_token import TOKEN
 
 class world_serpent(discord.Client):
@@ -36,7 +36,10 @@ class world_serpent(discord.Client):
                 await message.channel.send(SLIDE_GIF)
 
             if (message.content == "!luca"):
-                await message.channel.send(LUCA_GIF)
+                await message.channel.send(LUCA_PIC)
+
+            if (message.content == "!lucamog"):
+                await message.channel.send(LUCA_MOG_PIC)
             
             # create active bots
             if (message.content == "!startcsbot"):
@@ -71,7 +74,8 @@ WORLD_SERPENT_NAME = "Jörmungandr#9126"
 # GIF CONSTANRTS
 DONKEY_GIF = "https://cdn.discordapp.com/attachments/1136020852090093579/1323437159503630436/6VDRd5.gif?ex=67748267&is=677330e7&hm=916a303d2deec35b9d8e106c2c6b4d429014dcd21cd02754379dc64714eac39f&"
 SLIDE_GIF = "https://cdn.discordapp.com/attachments/1136020852090093579/1327483349308280934/cop-cop-slide.gif?ex=67833ab5&is=6781e935&hm=3950f1175aa01bb84b2ce1fc0a1e7c9585f8384e68fd6aae107c8509d0cc173a&"
-LUCA_GIF = "https://cdn.discordapp.com/attachments/375964855846305793/1372022987217436772/LucaBald.png?ex=6825437b&is=6823f1fb&hm=1c28d52d96c5539ae3dd714f3af9b47e4a0bec9f68914dbb2d72c7f94d29574e&"
+LUCA_PIC = "https://cdn.discordapp.com/attachments/375964855846305793/1381350838177366096/LucaBald.png?ex=684732b8&is=6845e138&hm=8f5446d9090301548ec06cdcbd1b4fefcb2f55a8f304e06cf0daf29152cb5435&"
+LUCA_MOG_PIC = "https://cdn.discordapp.com/attachments/375964855846305793/1381354448797958164/LucaMog.png?ex=68473614&is=6845e494&hm=efde3b65c2b99fc85166c2284cc43dc8022dc49ce3b53bdb4ec7ed285c1141b9&"
 
 client = world_serpent(intents=discord.Intents.all())
 client.run(TOKEN)
