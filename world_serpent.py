cd89ffe40483331e80eeb78dc9e6861bb8a29ef0
derektomassijr <derektomassi@me.com>
2025-06-24 21:40:36 -0400

    Added random will gif response

--- world_serpent.py
+++ world_serpent.py
@@ -1,11 +1,12 @@
 import discord
+import random
 import cs_bot
 from SiegeBotFiles import siege_bot
 from bot_token import TOKEN
 
 class world_serpent(discord.Client):
     # Global variables
-    channel = "test" if debug_on else "none_peasants"
+    channel = "test"
     active_bots = {}
     
     async def on_ready(self):
@@ -40,6 +41,10 @@ class world_serpent(discord.Client):
 
             if (message.content == "!lucamog"):
                 await message.channel.send(LUCA_MOG_PIC)
+
+            if (message.author.id == WILL_USER_ID and random.randint(1, 25) == 25):
+                await message.channel.send(WILL_GIF)
+
             
             # create active bots
             if (message.content == "!startcsbot"):
@@ -70,12 +75,14 @@ class world_serpent(discord.Client):
 
 # CONSTANTS
 WORLD_SERPENT_NAME = "Jörmungandr#9126"
+WILL_USER_ID = 752341726487904316
 
 # GIF CONSTANRTS
 DONKEY_GIF = "https://cdn.discordapp.com/attachments/1136020852090093579/1323437159503630436/6VDRd5.gif?ex=67748267&is=677330e7&hm=916a303d2deec35b9d8e106c2c6b4d429014dcd21cd02754379dc64714eac39f&"
 SLIDE_GIF = "https://cdn.discordapp.com/attachments/1136020852090093579/1327483349308280934/cop-cop-slide.gif?ex=67833ab5&is=6781e935&hm=3950f1175aa01bb84b2ce1fc0a1e7c9585f8384e68fd6aae107c8509d0cc173a&"
 LUCA_PIC = "https://cdn.discordapp.com/attachments/375964855846305793/1381350838177366096/LucaBald.png?ex=684732b8&is=6845e138&hm=8f5446d9090301548ec06cdcbd1b4fefcb2f55a8f304e06cf0daf29152cb5435&"
 LUCA_MOG_PIC = "https://cdn.discordapp.com/attachments/375964855846305793/1381354448797958164/LucaMog.png?ex=68473614&is=6845e494&hm=efde3b65c2b99fc85166c2284cc43dc8022dc49ce3b53bdb4ec7ed285c1141b9&"
+WILL_GIF = "https://tenor.com/view/autism-autistic-light-block-gif-lockin-gif-1402969396848104398"
 
 #GLOBAL FLAG -- Turn on if trying to test your code!!!
 debug_on = False
